d2ff7ae003f28d4e35ebcf0411e89fb50aa8ba60
s <infantyev48@gmail.com>
2021-04-18 04:51:09 +0300

    Replace fonts

--- js/main.js
+++ js/main.js
@@ -12,14 +12,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 btnOpenMenu.classList.remove('nav-btn--active')
                 sidebar.classList.remove('sidebar-active')
                 sidebarWrap.classList.remove('sidebar-wrap-active')
-                console.log("active has removed");
                 break;
 
             default:
                 btnOpenMenu.classList.add('nav-btn--active')
                 sidebar.classList.add('sidebar-active')
                 sidebarWrap.classList.add('sidebar-wrap-active')
-                console.log("active has added");
                 break;
         }
     });
